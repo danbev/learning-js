fd9a73e52a54a5ebbf3870d99772ff7e423e700d
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-08-03 10:48:14 +0200

    Adding insertion and selection sorts.

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -16,4 +16,31 @@ Algorithms.binarySearch = function(arr, key) {
     return -(low + 1);
 };
 
+Algorithms.insertionSort = function(arr) {
+    var n = arr.length, i, j;
+    for (i = 1; i < n; i++) {
+        for (j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+            var tmp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+    return arr;
+};
+
+Algorithms.selectionSort = function(arr) {
+    var n = arr.length, i, j, min;
+    for (i = 0; i < n; i++) {
+        min = i;
+        for (j = i; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        var tmp = arr[i];
+        arr[i] = arr[min]
+        arr[min] = tmp;
+    }
+    return arr;
+};
 
--- tests/algorithms/algorithms.js
+++ tests/algorithms/algorithms.js
@@ -4,8 +4,6 @@
 
     test('binary-search', function() {
         var arr = [1, 2, 3, 4, 5];
-        var core = Sync.Core();
-        var a = Algorithms.binarySearch(arr, 10);
         equal(Algorithms.binarySearch(arr, 1), 0, "BinarySearch for 1 should have index 0");
         equal(Algorithms.binarySearch(arr, 2), 1, "BinarySearch for 2 should have index 1");
         equal(Algorithms.binarySearch(arr, 3), 2, "BinarySearch for 3 should have index 2");
@@ -13,6 +11,16 @@
         equal(Algorithms.binarySearch(arr, 5), 4, "BinarySearch for 5 should have index 4");
         equal(Algorithms.binarySearch(arr, 6), -6, "BinarySearch for 6 should have index -6");
         equal(Algorithms.binarySearch(arr, 7), -6, "BinarySearch for 7 should have index -6");
+    });
+
+    test('insertion-sort', function() {
+        var sorted = Algorithms.insertionSort([5, 1, 2, 3, 4]);
+        deepEqual(sorted, [1, 2, 3, 4, 5], "The array should have been sorted using insertion-sort.");
+    });
+
+    test('selection-sort', function() {
+        var sorted = Algorithms.selectionSort([5, 1, 2, 3, 4]);
+        deepEqual(sorted, [1, 2, 3, 4, 5], "The array should have been sorted using selection-sort.");
     });
 
 })();

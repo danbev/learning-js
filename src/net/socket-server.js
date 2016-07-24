f4aa73c6c02315f236f63a04104df6e5335e8460
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-24 18:37:31 +0200

    Closing the socket and server upon first request
    Motivation:
    This can be handy when debugging node and wanting to put breakpoints in
    the shutdown of streams.

--- src/net/socket-server.js
+++ src/net/socket-server.js
@@ -3,7 +3,15 @@ var net = require('net');
 // telnet 127.0.0.1 7777
 var server = net.createServer(function(socket) {
   socket.write('Echo server\r\n');
-  socket.pipe(socket);
+  socket.on('data', function (data) {
+    socket.pipe(socket);
+    socket.end();
+    console.log('Client sent: ', data.toString());
+    server.close(function (err) {
+      console.log('Server closed err : ', err);
+    });
+  });
+
 });
 
 server.listen(7777, '127.0.0.1');

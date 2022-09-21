08a06cf3869928959d102444dab2c57a6e964825
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-09-21 13:38:51 +0200

    Add metadata dependencies parsing

--- paketo-buildpack-example/node/build.go
+++ paketo-buildpack-example/node/build.go
@@ -2,13 +2,36 @@ package node
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/paketo-buildpacks/packit/v2"
+	"github.com/BurntSushi/toml"
 )
 
 func Build() packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
-		fmt.Printf("BuildContext::CNBPATH: %s\n", context.CNBPath)
-		//fmt.Println("BuildContext::Plan:", context.Plan.node)
-		return packit.BuildResult{}, fmt.Errorf("node::Build shouldl always fail")
+		fmt.Printf("node::Build BuildContext::CNBPath: %s\n", context.CNBPath)
+
+		file, err := os.Open(filepath.Join(context.CNBPath, "buildpack.toml"))
+		if err != nil {
+			return packit.BuildResult{}, err
+		}
+
+		var anon_struct struct {
+			Metadata struct {
+				Dependencies []struct {
+					URI string `toml:"uri"`
+				} `toml:"dependencies"`
+			} `toml:"metadata"`
+		}
+		_, err = toml.DecodeReader(file, &anon_struct)
+		if err != nil {
+			return packit.BuildResult{}, err
+		}
+		uri := anon_struct.Metadata.Dependencies[0].URI
+		fmt.Printf("URI -> %s\n", uri)
+
+		return packit.BuildResult{}, fmt.Errorf("node::Build should always fail")
 	}
 }

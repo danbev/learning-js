54d39219b0bf16ccf09a7862fdf6a468691a2588
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-09-21 12:00:49 +0200

    Update id and log BuildContext CNBPath

--- paketo-buildpack-example/node/build.go
+++ paketo-buildpack-example/node/build.go
@@ -7,6 +7,8 @@ import (
 
 func Build() packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
+		fmt.Printf("BuildContext::CNBPATH: %s\n", context.CNBPath)
+		//fmt.Println("BuildContext::Plan:", context.Plan.node)
 		return packit.BuildResult{}, fmt.Errorf("node::Build shouldl always fail")
 	}
 }

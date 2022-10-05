0c91bb54ad78093a2e87fdf714047f1e45914e92
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-10-05 08:22:36 +0200

    Fix typos

--- src/generators/generator.js
+++ src/generators/generator.js
@@ -1,8 +1,8 @@
 module.exports = function* gen() {
   console.log("generator");
   var n = 0;
-  // pass the avlue of n as the value of next.value
-  // the value of v wil be the value passed in the next call to it.next(4)
+  // pass the value of n as the value of next.value
+  // the value of v will be the value passed in the next call to it.next(4)
   var v = yield n++;  
   return n + v;
 };

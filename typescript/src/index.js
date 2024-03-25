64c293277bd424b31695ecba0f6d3a8123dd4bfa
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-03-25 13:22:10 +0100

    src: update typescript example to be a ESM module
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- typescript/src/index.js
+++ typescript/src/index.js
@@ -1,13 +1,13 @@
-"use strict";
-exports.__esModule = true;
-function name(user) {
-    if (user === void 0) { user = { name: 'Fletch', age: 46 }; }
-    return "Hello " + user.name + "! ";
-}
-exports.name = name;
-var danbev = {
-    name: "Daniel",
-    age: 46
-};
-console.log(name());
-console.log(name(danbev));
+function greet(user) {                                                            
+    return `Hello ${user.name}!`;                                          
+}                                                                                
+                                                                                 
+export { greet };                                                                
+                                                                                 
+const danbev = {                                                                  
+    name: "Daniel",                                                              
+    age: 46                                                                      
+};                                                                               
+                                                                                 
+console.log(greet(danbev));
+
